42724b7c27f051868defbcc05ba084dc4963a1e5
Amrit kumar <126377548+amrit100612@users.noreply.github.com>
2026-08-20 13:31:50 +0530

    Time: 14 ms (26.25%) | Memory: 46.5 MB (34.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,21 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+    public boolean isAnagram(String s, String t){
+        if(s.length() != t.length()){
             return false;
         }
-        int chct[] = new int[26];
-        for (char c : s.toCharArray()) {
-            chct[c - 'a']++;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(char c : s.toCharArray()){
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-        for (char c : t.toCharArray()) {
-            if (chct[c - 'a'] == 0) {
+        for(char c : t.toCharArray()){
+            if(!hm.containsKey(c)){
                 return false;
             }
-            chct[c - 'a']--;
+            hm.put(c, hm.get(c) - 1);
+            if(hm.get(c) == 0){
+                hm.remove(c);
+            }
         }
-        return true;
+        return hm.isEmpty();
     }
 }
